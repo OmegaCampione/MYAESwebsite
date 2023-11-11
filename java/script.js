e7e02b42c77970093cbe26dd89ae681a8cc32ce6
Matheus Benjamim <143435071+OmegaCampione@users.noreply.github.com>
2023-11-11 20:19:51 -0300

    estou ficando Louco

--- java/script.js
+++ java/script.js
@@ -33,3 +33,28 @@ smallimg[2].onclick = function(){
 smallimg[3].onclick = function(){
   MainImg.src = smallimg[3].src;
 }
+
+//carrinho shenanigans:
+
+document.getElementById('adicionarAoCarrinhoBtn').addEventListener('click', function () {
+  // Obter os dados do produto
+  var produto = {
+      imagem: document.getElementById('MainImg').src,
+      nome: document.getElementById('textoprod1').textContent,
+      descricao: document.getElementById('textoprod2').textContent,
+      preco: document.getElementById('textoloja3').textContent,
+      tamanho: document.getElementById('tamanhoSelect').value,
+      quantidade: document.getElementById('quantidadeInput').value
+  };
+
+  // Obter o carrinho atual do localStorage
+  var carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+  // Adicionar o produto ao carrinho
+  carrinho.push(produto);
+
+  // Salvar o carrinho de volta no localStorage
+  localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+  alert('Produto adicionado ao carrinho!');
+});
