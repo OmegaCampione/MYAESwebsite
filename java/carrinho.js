3c764823ca447b835a008e7d5e525580870f19ba
Matheus Benjamim <143435071+OmegaCampione@users.noreply.github.com>
2023-11-11 21:32:39 -0300

    ...ratos me deixaram Louco...

--- java/carrinho.js
+++ java/carrinho.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     var cartTableBody = document.getElementById('cartItems');
+    var subtotalElement = document.getElementById('textocarrinhoSubtotal');
+    var totalElement = document.getElementById('textocarrinho5');
     var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
+    function calculateSubtotal(item) {
+        var quantity = parseInt(item.quantity);
+        var priceWithoutCurrency = item.price.replace('R$', '').replace(',', '.');
+    
+        if (!isNaN(priceWithoutCurrency) && !isNaN(quantity)) {
+            return parseFloat(priceWithoutCurrency) * quantity;
+        } else {
+            console.error("Valor de preço ou quantidade inválido:", item.price, quantity);
+            return 0; // Retorna 0 se houver um problema com os valores
+        }
+    }
+
     function renderCartItems() {
-        // Limpa a tabela antes de renderizar os itens do carrinho
         cartTableBody.innerHTML = '';
 
-        // Popula a tabela com os itens do carrinho
+        var subtotal = 0;
+
         cartItems.forEach(function (item, index) {
+            // Calcula o subtotal para o item
+            item.subtotal = calculateSubtotal(item);
+
             var row = document.createElement('tr');
             row.innerHTML = `
                 <td>${item.name}</td>
@@ -16,28 +33,32 @@ document.addEventListener('DOMContentLoaded', function () {
                 <td><img src="${item.image}" alt="${item.name}" style="max-width: 50px;"></td>
                 <td>${item.size}</td>
                 <td>${item.quantity}</td>
+                <td>R$ ${item.subtotal.toFixed(2)}</td>
                 <td><span class="remove-btn" data-index="${index}">Remover</span></td>
             `;
             cartTableBody.appendChild(row);
+
+            subtotal += item.subtotal;
         });
 
-        // Adiciona ouvinte de evento aos botões de remoção
-        var removeButtons = document.querySelectorAll('.remove-btn');
-        removeButtons.forEach(function (button) {
-            button.addEventListener('click', function () {
-                var itemIndex = this.getAttribute('data-index');
-                removeItemFromCart(itemIndex);
-                renderCartItems(); // Re-renderiza a tabela após a remoção
-            });
-        });
+        subtotalElement.innerText = `R$ ${subtotal.toFixed(2)}`;
+        totalElement.innerText = `R$ ${subtotal.toFixed(2)}`; // Atualiza o total da tabela de carrinho
     }
 
     function removeItemFromCart(index) {
-        // Remove o item do array e atualiza o localStorage
         cartItems.splice(index, 1);
         localStorage.setItem('cart', JSON.stringify(cartItems));
+        renderCartItems(); // Re-renderiza a tabela após a remoção
     }
 
-    // Inicializa a renderização dos itens do carrinho
     renderCartItems();
+
+    // Adiciona ouvinte de evento aos botões de remoção
+    var removeButtons = document.querySelectorAll('.remove-btn');
+    removeButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            var itemIndex = this.getAttribute('data-index');
+            removeItemFromCart(itemIndex);
+        });
+    });
 });
--- java/script.js
+++ java/script.js
@@ -49,36 +49,52 @@ document.addEventListener('DOMContentLoaded', function () {
       var selectedSize = document.querySelector('select').value;
       var quantity = document.querySelector('input[type="number"]').value;
 
-      // Cria um objeto contendo os dados do produto
-      var productData = {
-          name: productName,
-          description: productDescription,
-          price: productPrice,
-          image: productImage,
-          size: selectedSize,
-          quantity: quantity
-      };
-
       // Verifica se o localStorage é suportado pelo navegador
-      if (typeof(Storage) !== "undefined") {
+      if (typeof (Storage) !== "undefined") {
           // Recupera os dados existentes do carrinho do localStorage, se houver
           var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
-          // Adiciona os dados do produto ao carrinho
-          cartItems.push(productData);
+          // Verifica se o produto já está no carrinho
+          var existingProductIndex = findProductIndex(cartItems, productName, selectedSize);
+
+          if (existingProductIndex !== -1) {
+              // Se o produto já está no carrinho, atualiza a quantidade
+              cartItems[existingProductIndex].quantity = parseInt(cartItems[existingProductIndex].quantity) + parseInt(quantity);
+          } else {
+              // Se o produto não está no carrinho, adiciona normalmente
+              var productData = {
+                  name: productName,
+                  description: productDescription,
+                  price: productPrice,
+                  image: productImage,
+                  size: selectedSize,
+                  quantity: quantity
+              };
+              cartItems.push(productData);
+          }
 
           // Atualiza o carrinho no localStorage
           localStorage.setItem('cart', JSON.stringify(cartItems));
 
           // Alerta o usuário que o produto foi adicionado ao carrinho (você pode modificar isso conforme necessário)
           alert('Produto adicionado ao carrinho');
-
       } else {
           // Caso o localStorage não seja suportado
           alert('Desculpe, seu navegador não suporta o recurso de carrinho de compras.');
       }
   });
+
+  // Função para encontrar o índice de um produto no carrinho
+  function findProductIndex(cartItems, productName, selectedSize) {
+      for (var i = 0; i < cartItems.length; i++) {
+          if (cartItems[i].name === productName && cartItems[i].size === selectedSize) {
+              return i;
+          }
+      }
+      return -1;
+  }
 });
+
 
 //carrinho load
 
