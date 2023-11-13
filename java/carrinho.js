34f4cb149e0ef7e3eaf95a7b5fcd615dba458733
Matheus Benjamim <143435071+OmegaCampione@users.noreply.github.com>
2023-11-13 16:37:57 -0300

    a

--- java/carrinho.js
+++ java/carrinho.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Seletores / variáveis
     var cartTableBody = document.getElementById('cartItems');
     var subtotalElement = document.getElementById('textocarrinhoSubtotal');
     var totalElement = document.getElementById('textocarrinho5');
+    var appliedDiscount = 0;
     var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
+    // Função para calcular o subtotal de um item
     function calculateSubtotal(item) {
         var quantity = parseInt(item.quantity);
         var priceWithoutCurrency = item.price.replace('R$', '').replace(',', '.');
@@ -12,19 +15,19 @@ document.addEventListener('DOMContentLoaded', function () {
             return parseFloat(priceWithoutCurrency) * quantity;
         } else {
             console.error("Valor de preço ou quantidade inválido:", item.price, quantity);
-            return 0; // Retorna 0 se houver um problema com os valores
+            return 0;
         }
     }
 
+    // Função para renderizar os itens no carrinho
     function renderCartItems() {
         cartTableBody.innerHTML = '';
-
+    
         var subtotal = 0;
-
+    
         cartItems.forEach(function (item, index) {
-            // Calcula o subtotal para o item
             item.subtotal = calculateSubtotal(item);
-
+    
             var row = document.createElement('tr');
             row.innerHTML = `
                 <td>${item.name}</td>
@@ -37,23 +40,34 @@ document.addEventListener('DOMContentLoaded', function () {
                 <td><span class="remove-btn" data-index="${index}">Remover</span></td>
             `;
             cartTableBody.appendChild(row);
-
+    
             subtotal += item.subtotal;
         });
+    
+           // Calcula o desconto aplicado
+    var discountAmount = subtotal * appliedDiscount;
 
-        subtotalElement.innerText = `R$ ${subtotal.toFixed(2)}`;
-        totalElement.innerText = `R$ ${subtotal.toFixed(2)}`; // Atualiza o total da tabela de carrinho
-    }
+    // Exibe o desconto na tabela como negativo
+    document.getElementById('discountAmount').innerText = `- R$ ${Math.abs(discountAmount).toFixed(2)}`;
 
+    // Aplica o desconto
+    var totalWithDiscount = subtotal - discountAmount;
+    totalElement.innerText = `R$ ${totalWithDiscount.toFixed(2)}`;
+
+    subtotalElement.innerText = `R$ ${subtotal.toFixed(2)}`;
+}
+
+    // Função para remover um item do carrinho
     function removeItemFromCart(index) {
         cartItems.splice(index, 1);
         localStorage.setItem('cart', JSON.stringify(cartItems));
         renderCartItems(); // Re-renderiza a tabela após a remoção
     }
 
+    // Renderiza os itens inicialmente
     renderCartItems();
 
-    // Adiciona ouvinte de evento aos botões de remoção
+    // Add evento aos botões de remoção
     var removeButtons = document.querySelectorAll('.remove-btn');
     removeButtons.forEach(function (button) {
         button.addEventListener('click', function () {
@@ -61,4 +75,93 @@ document.addEventListener('DOMContentLoaded', function () {
             removeItemFromCart(itemIndex);
         });
     });
+
+    // Cupom
+    var appliedDiscount = 0;
+
+    function applyDiscountAndShowWarning(discount, renderFunction, warningElement, message) {
+        function clearWarning() {
+            // Ajusta a opacidade para esconder o aviso
+            warningElement.style.opacity = 0;
+
+            // Limpa o aviso após a transição de saída
+            setTimeout(function () {
+                warningElement.innerText = '';
+            }, 300);
+        }
+
+        if (discount > appliedDiscount) {
+            appliedDiscount = discount;
+            renderFunction();
+            warningElement.innerText = message;
+
+            // Ajusta a opacidade para exibir o aviso
+            warningElement.style.opacity = 1;
+
+            // Limpa o aviso após 3 segundos
+            setTimeout(clearWarning, 3000);
+        } else {
+            warningElement.innerText = "Desconto menor ou igual ao atual.";
+
+            // Ajusta a opacidade para exibir o aviso
+            warningElement.style.opacity = 1;
+
+            // Limpa o aviso após 3 segundos
+            setTimeout(clearWarning, 3000);
+        }
+    }
+
+    // Função para aplicar desconto usando um cupom
+    function applyCoupon() {
+        var couponInput = document.getElementById('placeholdercupom');
+        var couponCode = couponInput.value;
+        var couponWarning = document.getElementById('couponWarning');
+    
+        // Lógica para verificar e aplicar o desconto com base no código do cupom
+        switch (couponCode) {
+            case 'FAVIP':
+                applyDiscountAndShowWarning(
+                    0.05, // desconto para 'FAVIP'
+                    renderCartItems,
+                    couponWarning,
+                    "Desconto aplicado: 5%"
+                );
+                break;
+            case 'MY10':
+                applyDiscountAndShowWarning(
+                    0.10, // desconto para 'MY10'
+                    renderCartItems,
+                    couponWarning,
+                    "Desconto aplicado: 10%"
+                );
+                break;
+            case 'EXPOTECH':
+                applyDiscountAndShowWarning(
+                    0.20, // desconto para 'EXPOTECH'
+                    renderCartItems,
+                    couponWarning,
+                    "Desconto aplicado: 20%"
+                );
+                break;
+            default:
+                couponWarning.innerText = "Cupom inválido.";
+    
+                // Ajusta a opacidade para exibir o aviso
+                couponWarning.style.opacity = 1;
+    
+                // Limpa o aviso após 3 segundos
+                setTimeout(function () {
+                    couponWarning.innerText = '';
+                    couponWarning.style.opacity = 0;
+                }, 3000);
+                break;
+        }
+    
+        // Limpa o campo de cupom após verificar os cupons
+        couponInput.value = '';
+    }
+
+    // Add evento ao botão de aplicar cupom
+    var applyCouponButton = document.getElementById('textobutcupom2');
+    applyCouponButton.addEventListener('click', applyCoupon);
 });
